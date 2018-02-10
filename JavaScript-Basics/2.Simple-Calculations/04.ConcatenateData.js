2218c8ab08983188cda468fd4d3aaa56fe41a713
“[plamen1982]” <plamen.hristov@ymail.com>
2018-02-10 17:09:39 -0500

    JS Basics: 2.Simple-Calculations: 04.ConcatenateData soution

---
+++ JavaScript-Basics/2.Simple-Calculations/04.ConcatenateData.js
@@ -0,0 +1,6 @@
+function concatenateData([firstName, lastName, age, town]) {
+    console.log(`You are ${firstName} ${lastName}, a ${age}-years old person from ${town}.`)
+}
+
+concatenateData('Plamen', 'Hristov', 35, 'Dupnitsa')
+
