b58642201c4d686bc4e496e5b562aa1df687d68f
“[plamen1982]” <plamen.hristov@ymail.com>
2018-02-14 11:37:20 -0500

    JS Basics: 2.Simple-Calculations: 14.TrainingLabSeats solution

---
+++ JavaScript-Basics/2.Simple-Calculations/14.TrainingLabSeats.js
@@ -0,0 +1,13 @@
+function trainingLabSeats([l, w]) {
+    let lengthOfSeat = 1.2;
+    let widthOfSeat = 0.7;
+    let widthOfCorridor = 1;
+    let numberOfSeatsOnLength = Math.floor(l / lengthOfSeat)
+    let widthWithoutCorridor = w - widthOfCorridor;
+    let numberOfSeathOnWidth = Math.floor(widthWithoutCorridor / widthOfSeat)
+    let numberOfSeats = (numberOfSeathOnWidth * numberOfSeatsOnLength) - 3;
+    console.log(numberOfSeats)
+}
+
+trainingLabSeats([15, 8.9])
+trainingLabSeats([8.4, 5.2])
