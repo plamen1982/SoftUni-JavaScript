e8d6adaae90ae22aac12f06c51760b708907e805
“[plamen1982]” <plamen.hristov@ymail.com>
2018-02-17 21:03:33 -0500

    JS Basics: 3.Simple-Conditions: 4.GreaterNumber.js solution

---
+++ JavaScript-Basics/3.Simple-Conditions/4.GreaterNumber.js
@@ -0,0 +1,16 @@
+function findGreaterNumber([arg1, arg2]) {
+    const number1 = Number(arg1);
+    const number2 = Number(arg2);
+    if(number1 > number2) {
+        console.log(number1)
+    } else if(number1 < number2) {
+        console.log(number2)
+    } else {
+        console.log(number1)
+    }
+}
+
+findGreaterNumber([5, 3]);
+findGreaterNumber([3, 5]);
+findGreaterNumber([10, 10]);
+findGreaterNumber([-5, 5]);
