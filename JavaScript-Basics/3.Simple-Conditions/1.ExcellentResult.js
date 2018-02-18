ac56c16e21600ddf0e1772628f1c18c02dbe2e0c
“[plamen1982]” <plamen.hristov@ymail.com>
2018-02-17 20:45:20 -0500

    JS Basics: 3.Simple-Conditions: 1.ExcellentResult.js solution

---
+++ JavaScript-Basics/3.Simple-Conditions/1.ExcellentResult.js
@@ -0,0 +1,10 @@
+function isExcellent([arg]) {
+    if(arg >= 5.50) {
+        console.log('Excellent!')
+    }
+}
+
+isExcellent([6]);
+isExcellent([5]);
+isExcellent([5.50]);
+isExcellent([5.49]);
