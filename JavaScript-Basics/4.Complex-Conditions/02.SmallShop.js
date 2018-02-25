08d1c53a242223e44d65c637a9880d3f7bca0892
“[plamen1982]” <plamen.hristov@ymail.com>
2018-02-24 19:17:39 -0500

    4.Complex-Conditions:02.SmallShop.js solution

---
+++ JavaScript-Basics/4.Complex-Conditions/02.SmallShop.js
@@ -0,0 +1,65 @@
+function smallShop([nameItem, city, numberItems]) {
+    numberItems = Number(numberItems);
+
+    if(city === 'Sofia') {
+        switch(nameItem) {
+            case 'coffee':
+                console.log( numberItems * 0.5 );
+                break;
+            case 'water':
+                console.log( numberItems * 0.8 );
+                break;
+            case 'beer':
+                console.log( numberItems * 1.2 );
+                break;
+            case 'sweets':
+                console.log( numberItems * 1.45 );
+                break;
+            case 'peanuts':
+                console.log( numberItems * 1.6 );
+                break;
+        }
+    } else if(city === 'Plovdiv') {
+        switch(nameItem) {
+            case 'coffee':
+                console.log( numberItems * 0.4 );
+                break;
+            case 'water':
+                console.log( numberItems * 0.7 );
+                break;
+            case 'beer':
+                console.log( numberItems * 1.15 );
+                break;
+            case 'sweets':
+                console.log( numberItems * 1.3 );
+                break;
+            case 'peanuts':
+                console.log( numberItems * 1.5 );
+                break;
+        }
+    } else if(city === 'Varna') {
+        switch(nameItem) {
+            case 'coffee':
+                console.log( numberItems * 0.45 );
+                break;
+            case 'water':
+                console.log( numberItems * 0.7 );
+                break;
+            case 'beer':
+                console.log( numberItems * 1.1 );
+                break;
+            case 'sweets':
+                console.log( numberItems * 1.35 );
+                break;
+            case 'peanuts':
+                console.log( numberItems * 1.55 );
+                break;
+        }
+    }
+}
+
+smallShop(['coffee', 'Varna', 2]);
+smallShop(['peanuts', 'Plovdiv', 1]);
+smallShop(['beer', 'Sofia', 6]);
+smallShop(['water', 'Plovdiv', 3]);
+smallShop(['sweets', 'Sofia', 2.23]);
